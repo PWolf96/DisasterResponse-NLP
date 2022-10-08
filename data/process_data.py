801a8c295cc25e57c67da18b78ed4aaf1f39a5d3
PetarV <pv1g15@soton.ac.uk>
2022-10-08 20:07:11 +0100

    Initial commit

---
+++ data/process_data.py
@@ -0,0 +1,126 @@
+#Importing relevant libraries
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    '''
+    A method to load the CSV files and return dataframe objects
+    Args:
+    Messages_filepath - the filepath of the messages csv
+    categories_filepath - the filepath of the categories csv
+
+    Ouputs
+    messages - dataframe
+    categories - dataframe
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    
+    
+    return messages, categories
+
+def clean_data(messages, categories):  
+    '''
+    A method to clean the passed datasets and combine them into a single one. 
+    Cleaning refers to:
+        - Removal of duplicate values
+        - Converting the categories inputs into separate columns with their respective values
+        - Removing messages which are not associated with any categories
+        - Combining both messages and categories into a single dataset to be used for training
+
+    Args:
+    messages - dataframe
+    categories - dataframe
+
+    Outputs:
+    df - a combined cleaned dataframe 
+    '''
+
+    #Drop duplicate values based on the "id" column
+    categories = categories.drop_duplicates(subset=(["id"]), inplace=False)
+    messages = messages.drop_duplicates(subset=(["id"]), inplace=False)
+        
+    #Split the column "categories" into separate values 
+    categoriesSplit = categories["categories"].str.split(";", expand=True)
+
+    
+    #Select the first row of the new dataframe
+    row = categoriesSplit.iloc[0]
+
+    category_colnames = []
+    #Split each string and append the category name into a list
+    for name in row:
+        col = name.split("-")[0]
+        category_colnames.append(col)
+        
+        
+    #Rename the columns to represent their category name
+    categoriesSplit.columns = category_colnames  
+    
+    #Iterate through all values and keep only the numbers. Then convert them to integers
+    for column in categoriesSplit:
+        categoriesSplit[column] = categoriesSplit[column].str[-1]
+        categoriesSplit[column] = categoriesSplit[column].astype(int)
+    
+    #Adding the id column to categoriesSplit
+    categoriesSplit["id"] = categories["id"]
+    
+    #Merge "messages" with "categoriesSplit" into one dataframe based on "id"
+    df = pd.merge(messages, categoriesSplit, on=['id'])
+    
+    #drop any rows and columns which have only 0s
+    #columns
+    df = df.loc[:, (df != 0).any(axis=0)]
+    #rows
+    df = df[~(df.loc[:, 'related':'direct_report'] == 0).all(axis=1)]
+    
+    return df
+
+
+def save_data(df, database_filename):
+    '''
+    A method to save the dataframe into a sql database
+
+    Args:
+    df - the cleaned dataframe
+    database_filename - the name of the database
+
+    Output:
+    SQL database containing the dataframe values
+    '''
+    filenameDb = database_filename.split("/")[1]
+    engine = create_engine('sqlite:///' + filenameDb)
+    filename = filenameDb.split(".")[0]
+    df.to_sql(filename, engine, index=False)  
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}\n  DATABASE: {}'
+              .format(messages_filepath, categories_filepath, database_filepath))
+        messages, categories = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(messages, categories)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,137 @@
+# import libraries
+import sys
+import re
+import pandas as pd
+import numpy as np
+
+import pickle
+
+import sqlite3
+from sqlalchemy import create_engine
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'omw-1.4'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+
+def load_data(database_filepath):
+    '''
+    A method to load the data from a sql file into a dataframe and define values needed
+    for data training
+
+    Args:
+    database_filepath - the filepath of the sql file
+
+    Output:
+    X - a list of all messages as strings
+    y - the dataframe with its respective values
+    '''
+    # load data from database
+    filenameDb = database_filepath.split("/")[1]
+    engine = create_engine('sqlite:///' + filenameDb)
+    conn = sqlite3.connect(filenameDb)
+    filename = filenameDb.split(".")[0]
+    
+    #Read the sql database into a dataframe
+    df = pd.read_sql('SELECT * FROM ' + filename, conn)
+    X = df.message.values
+    y = df.drop(["id","message","genre","original","related"], axis=1).values
+    
+    return X, y, df.columns.tolist()
+
+
+def tokenize(text):
+    '''
+    A method to analyze the messages as blocks of text. To split the words and use their root forms.
+    Then create a list of "tokens" which consist of the root form of each word and its role in a sentence
+
+    Args:
+    text - the sentences
+
+    Outputs:
+    clean_tokens - a list consisting of the root form of each word in lower case and its role in a sentence 
+    '''
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    '''
+    A method that creates a pipeline for building a model to classify messages.
+    It consists of the following:
+        - CountVectorizer - creating the text documents into a matrix of token counts
+        - TfIdfTransformer - converting the text documents intoa  matrix of tf-idf features
+        - RandomForsetClassifier - classifying the dataset using RandomForests
+    '''
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(LogisticRegression()))
+    ])
+
+
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    '''
+    A method to evaluate the classification model
+    '''
+    # predict on test data
+    y_pred = model.predict(X_test)
+
+
+def save_model(model, model_filepath):
+    '''
+    A method to save the model into a pickle file
+    '''
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
